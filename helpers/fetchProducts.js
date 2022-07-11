7c0e6fd1b2630bae771a843751ed62567f9866df
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-07-11 17:46:54 -0300

    Primeiro requisito

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,10 @@
 const fetchProducts = () => {
   // seu código aqui
+  const site = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(site)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Falhou \n${error}`))
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,4 +6,17 @@ describe('1 - Teste a função fetchProducts', () => {
   it('Should be a function', () => {
     expect(typeof(fetchProducts)).toEqual('function');
   })
+
+  it('Call fetch when "fetchProducts" function is called as a function', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  })
+
+  it('Test if the fetchProducts function return with the "computador" argument is a data structure equal to the computerSearch object, which is already imported in the file.', () => {
+    fetchProducts('computador');
+  })
+
+  it('Test if calling the fetchProducts function with no argument returns an error with the message: "You must provide an url"', () => {
+    fetchProducts('');
+  })
 });
